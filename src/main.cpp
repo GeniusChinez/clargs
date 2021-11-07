042b3cb90f39fbbd0757f6c5f1ed533fa8cf85dd
geniuschinez <geniuschinembiri@gmail.com>
2021-11-07 08:06:56 +0200

    Reduced indentation to 2 spaces

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <iostream>
 
 int main(int argc, char* const argv[]) {
-    auto args = clargs::get_arguments(argc, argv);
-    const auto& naked_values = args.get_naked_values();
+  auto args = clargs::get_arguments(argc, argv);
+  const auto& naked_values = args.get_naked_values();
 
-    for (const auto& value : naked_values) {
-        std::cout <<  "naked: " << value << "\n";
-    }
+  for (const auto& value : naked_values) {
+    std::cout <<  "naked: " << value << "\n";
+  }
 }
